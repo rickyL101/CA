44a24dca1c81b1be7d2aa070b4d25db97811c317
rickyL101 <rickylane33@yahoo.com>
2021-05-07 21:40:21 +0100

    signup route and html file added

--- main.py
+++ main.py
@@ -8,5 +8,12 @@ def home():
     return render_template("index.html")
 
 
+@app.route('/signup')
+def home():
+    return render_template("signup.html")
+
+
+
+
 if __name__ == '__main__':
     app.run(debug=True)
