b2ba29c0d659fae6f48195c498e2618b274ae128
rickyL101 <rickylane33@yahoo.com>
2021-05-08 15:51:49 +0100

    added home page when signed in

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__, static_url_path='/static', static_folder='static/')
 
 
+
 @app.route('/')
-def home():
-    return render_template("index.html")
+def signin():
+    return render_template("signin.html")
 
 
 @app.route('/signup')
@@ -13,6 +14,9 @@ def signup():
     return render_template("signup.html")
 
 
+@app.route('/home')
+def home():
+    return render_template("home.html")
 
 
 if __name__ == '__main__':
