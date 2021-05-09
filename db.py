d1d88edbb14a701551e23cfc04799a236d8857cb
rickyL101 <rickylane33@yahoo.com>
2021-05-10 00:29:06 +0100

    changed to remote mysql database succesfully conected and created a table

--- db.py
+++ db.py
@@ -1,23 +1,16 @@
-import pypyodbc
+#https://stackoverflow.com/questions/8932261/create-mysqldb-database-using-python-script/25621844
+
+import mysql.connector
 
 with open(".pw") as f:
-  password = f.read()
+    password = f.read()
 
-connection = connection = pypyodbc.connect('Driver={ODBC Driver 17 for SQL Server};'
-                                           'Server=137.135.135.105;'
-                                           'Database=master;'
-                                           'encrypt=yes;'
-                                           'TrustServerCertificate=yes;'
-                                           'UID=sa;'
-                                           'PWD=' + password, autocommit=True)
-
+#establish connection to remote database
+connection = mysql.connector.connect(host="remotemysql.com", user="Doc0XDVVtG", password="" + password, database="Doc0XDVVtG")
 cursor = connection.cursor()
-def createDB():  
-    SQLCommand = ("IF EXISTS(SELECT name FROM master.dbo.sysdatabases WHERE name = N'userData')BEGIN SELECT 'Database Name already Exist' AS Message END ELSE BEGIN CREATE DATABASE [userData] SELECT 'New Database is Created' END;")
-    cursor.execute(SQLCommand) 
-    SQLCommand = ("IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES T WHERE T.TABLE_SCHEMA = 'dbo' AND T.TABLE_NAME = 'users') BEGIN CREATE TABLE dbo.users(user_id INT IDENTITY(1, 1),name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,password VARCHAR(255) NOT NULL,PRIMARY KEY(user_id)) END;")
-    cursor.execute(SQLCommand)
-    print('done')
+#create the table(s) required 
 
+def createDB():
+    cursor.execute("create table IF NOT EXISTS users(user_id INT NOT NULL AUTO_INCREMENT,name varchar(100) NOT NULL,email varchar(100) NOT NULL,password varchar(100) NOT NULL,PRIMARY KEY(user_id));")
 
 
--- main.py
+++ main.py
@@ -1,68 +1,66 @@
-from flask import Flask, render_template, request,session, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 import os
 import db
-
 app = Flask(__name__)
 
-with open(".pw") as f:
-  password = f.read()
-
-app = Flask(__name__, static_url_path='/static', static_folder='static/')
 # https://stackoverflow.com/questions/34902378/where-do-i-get-a-secret-key-for-flask/34903502
 app.SECRET_KEY = os.urandom(24)
 
 
 #base page
-@app.route('/')
+@app.route("/")
 def signin():
     return render_template("signin.html")
 
-#user signup
-@app.route('/signup')
+#signup page
+@app.route("/signup")
 def signup():
     return render_template("signup.html")
 
-@app.route('/signout')
+
+@app.route("/logout")
 def signout():
     session.pop('user_id')
-    return red
+    return redirect("/")
 
 #user signed in
 @app.route('/home')
 def home():
     if 'user_id' in session:
-        return render_template ("home.html")
+        return render_template("home.html")
     else:
         return redirect("/")
 
 #to validate user login
-@app.route('/validation', methods=['POST'])
+@app.route("/validation", methods=["POST"])
 def validation():
     email = request.form.get('email')
     password = request.form.get('password')
-    db.cursor.execute(f"""Select * FROM users WHERE email LIKE '{email}' AND password LIKE '{password}'""")
-    users = cursor.fetchall()
+    db.cursor.execute(f"""Select * FROM `users` WHERE `email` LIKE '{email}' AND `password` LIKE '{password}';""")
+    users = db.cursor.fetchall()
     if len(users) > 0:
         session['user_id'] = users[0][0]
-        return redirect("home.html")
+        return redirect("/home")
     else:
-        return redirect("login.html")
+        return redirect("/")
+        flash("Invalid Email or Password", category="error")
 
 #add a new user
-
 @app.route('/add', methods=['POST'])
 def add():
-    name = request.form.get('user_name')
-    email = request.form.get('user_email')
-    password = request.form.get('user_password')
-    s = f'''INSERT INTO students(id, name, email, password) VALUES (NULL, '{user_name}', '{user_email}', '{user_password}';'''
+    #below block taken from moodle "Setup and example server"
+    name = request.form.get('name')
+    email = request.form.get('email')
+    password = request.form.get('password')
+    s = f'''INSERT INTO users(id, name, email, password) VALUES (NULL, '{name}', '{email}', '{password}');'''
     db.cursor.execute(s)
     db.cursor.connection.commit()
+
     new_user = db.cursor.fetchall()
     session['user_id'] = new_user[0][0]
     return redirect('/home')
-
+    
 if __name__ == '__main__':
     db.createDB()
-    app.run(host='0.0.0.0', port='8080',ssl_context=('cert.pem', 'privkey.pem'))
-    
+    app.run(debug=True)
+
