c71c422457564541676f1b62af70f9ddd840b9e3
rickyL101 <rickylane33@yahoo.com>
2021-05-09 17:43:56 +0100

    updated database creation added session data to main

--- db.py
+++ db.py
@@ -13,9 +13,9 @@ def createDB():
                                             'PWD=' + password, autocommit=True)
 
     cursor = connection.cursor()
-    SQLCommand = ("CREATE DATABASE IF NOT EXISTS user_data;")
-    cursor.execute(SQLCommand)
-    SQLCommand = ("CREATE TABLE users(user_id INT AUTO_INCREMENT,name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,password VARCHAR(255) NOT NULL,PRIMARY KEY(user_id);")
+    SQLCommand = ("IF EXISTS(SELECT name FROM master.dbo.sysdatabases WHERE name = N'userData')BEGIN SELECT 'Database Name already Exist' AS Message END ELSE BEGIN CREATE DATABASE [userData] SELECT 'New Database is Created' END;")
+    cursor.execute(SQLCommand)  
+    SQLCommand = ("IF NOT EXISTS (SELECT 1 FROM INFORMATION_SCHEMA.TABLES T WHERE T.TABLE_SCHEMA = 'dbo' AND T.TABLE_NAME = 'users') BEGIN CREATE TABLE dbo.users(user_id INT IDENTITY(1, 1),name VARCHAR(255) NOT NULL,email VARCHAR(255) NOT NULL,password VARCHAR(255) NOT NULL,PRIMARY KEY(user_id)) END;")
     cursor.execute(SQLCommand)
     print('done')
 
--- main.py
+++ main.py
@@ -1,10 +1,13 @@
-from flask import Flask, render_template, request
-import mysql.connector
+from flask import Flask, render_template, request,session, redirect
+import os
 import db
+
 app = Flask(__name__, static_url_path='/static', static_folder='static/')
 
-connection = mysql.connector.connect(host="137.135.135.105", username="azureuser", database="userData")
-cursor = connection.cursor()
+# https://stackoverflow.com/questions/34902378/where-do-i-get-a-secret-key-for-flask/34903502
+app.SECRET_KEY = os.urandom(24)
+
+
 #base page
 @app.route('/')
 def signin():
@@ -15,32 +18,49 @@ def signin():
 def signup():
     return render_template("signup.html")
 
+@app.route('/signout')
+def signout():
+    session.pop('user_id')
+    return red
+
 #user signed in
 @app.route('/home')
 def home():
-    return render_template("home.html")
+    if 'user_id' in session:
+        return render_template ("home.html")
+    else:
+        return redirect("/")
 
 #to validate user login
 @app.route('/validation', methods=['POST'])
 def validation():
     email = request.form.get('email')
     password = request.form.get('password')
-    cursor.execute(f"""Select * FROM `users` WHERE `email` LIKE '{email}' AND `password` LIKE '{password}'""")
+    cursor.execute(f"""Select * FROM users WHERE email LIKE '{email}' AND password LIKE '{password}'""")
     users = cursor.fetchall()
     if len(users) > 0:
-        return render_template("home.html")
+        session['user_id'] = users[0][0]
+        return redirect("home.html")
     else:
-        return render_template("login.html")
+        return redirect("login.html")
 
 #add a new user
-@app.route('/add')
+
+@app.route('/add', methods=['POST'])
 def add():
     name = request.form.get('user_name')
     email = request.form.get('user_email')
     password = request.form.get('user_password')
-    cursor.execute(f"""Insert INTO `users` (`user_id`, `name`,`email`,`password`) VALUES (NULL, '{name}', '{email}', '{password}')""")
+    cur = mysql.connection.cursor()
+    s = f'''INSERT INTO students(id, name, email, password) VALUES (NULL, '{user_name}', '{user_email}', '{user_password}';'''
+    cur.execute(s)
+    mysql.connection.commit()
+
+    new_user = cur.fetchall()
+    session['user_id'] = new_user[0][0]
+    return redirect('/home')
 
 if __name__ == '__main__':
     db.createDB()
-    app.run(host='0.0.0.0', port='80')
+    app.run()
     
